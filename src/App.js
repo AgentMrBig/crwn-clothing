606fde4c52163f1776630cab4266a7e568ef644b
Eric Hendrix <agentmrbig@gmail.com>
2020-04-07 00:05:36 -0400

    Added BrowserRouter Swith and Route, Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Test from './pages/test/test.component';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route path="/test" component={Test} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/test/test.component.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Test = () => (
+  <div>
+    <h1>TEST</h1>
+  </div>
+);
+
+export default Test;
